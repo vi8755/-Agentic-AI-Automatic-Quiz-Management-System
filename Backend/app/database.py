ff3ba9bfea411aa6c8262a618a81bd329ca83c91
vishal kumar <vk9491015@gmail.com>
2026-07-11 02:49:23 +0530

    Add PostgreSQL driver

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -5,18 +5,18 @@ from .config import settings
 
 DATABASE_URL = settings.DATABASE_URL
 
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
-
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False},
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine,
 )
-
 
 Base = declarative_base()
